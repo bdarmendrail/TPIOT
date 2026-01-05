6901a570a2e264ec6062b12f4e6231591885a4e5
bdarmendrail <benjamin.darmendrail@etud.univ-pau.fr>
2026-01-05 16:39:37 +0100

    Test Main

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,7 +1,16 @@
 package com.example;
+import java.util;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        switch(choix){
+            case 1:
+                //Activation du patient
+                Scanner scP = new Scanner(System.in);
+                for(int i = 0;i<count(lesPatients);i++){
+                    print(st)
+                }
+        }
     }
 }
