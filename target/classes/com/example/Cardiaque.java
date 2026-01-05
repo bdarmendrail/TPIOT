eaac0803e38b3b4b8e8eb18018112f0c4bd2c40f
LOTZ <william.lotz64@gmail.com>
2026-01-05 16:19:18 +0100

    Cardiaque

---
+++ target/classes/com/example/Cardiaque.java
@@ -0,0 +1,17 @@
+public class Cardiaque {
+    
+    private double frequenceCardiaque;
+
+    public Cardiaque( double frequenceCardiaque) {
+       
+        this.frequenceCardiaque = frequenceCardiaque;
+    }
+
+    public double getFrequenceCardiaque() {
+        return frequenceCardiaque;
+    }
+
+    public void setFrequenceCardiaque(double frequenceCardiaque) {
+        this.frequenceCardiaque = frequenceCardiaque;
+    }
+}
